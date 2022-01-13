ed8284e4197c588a4368655a8c25844abd956bdd
KatiaJTartari <70042571+KatiaJTartari@users.noreply.github.com>
2022-01-12 21:04:20 -0300

    Add files via upload

---
+++ CadCliente/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿
+namespace CadCliente
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.mskCpf = new System.Windows.Forms.MaskedTextBox();
+            this.mskCep = new System.Windows.Forms.MaskedTextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.txtComplemento = new System.Windows.Forms.TextBox();
+            this.mskTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblCpf = new System.Windows.Forms.Label();
+            this.lblCep = new System.Windows.Forms.Label();
+            this.lblEndereco = new System.Windows.Forms.Label();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.lblComplemento = new System.Windows.Forms.Label();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnExibir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(82, 41);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(81, 20);
+            this.txtCodigo.TabIndex = 0;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(82, 88);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(435, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // mskCpf
+            // 
+            this.mskCpf.Location = new System.Drawing.Point(578, 85);
+            this.mskCpf.Mask = "000,000,000-00";
+            this.mskCpf.Name = "mskCpf";
+            this.mskCpf.Size = new System.Drawing.Size(98, 20);
+            this.mskCpf.TabIndex = 2;
+            // 
+            // mskCep
+            // 
+            this.mskCep.Location = new System.Drawing.Point(82, 134);
+            this.mskCep.Mask = "00000-000";
+            this.mskCep.Name = "mskCep";
+            this.mskCep.Size = new System.Drawing.Size(100, 20);
+            this.mskCep.TabIndex = 3;
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(268, 141);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(429, 20);
+            this.txtEndereco.TabIndex = 4;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(82, 183);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(72, 20);
+            this.txtNumero.TabIndex = 5;
+            // 
+            // txtComplemento
+            // 
+            this.txtComplemento.Location = new System.Drawing.Point(268, 186);
+            this.txtComplemento.Name = "txtComplemento";
+            this.txtComplemento.Size = new System.Drawing.Size(249, 20);
+            this.txtComplemento.TabIndex = 6;
+            // 
+            // mskTelefone
+            // 
+            this.mskTelefone.Location = new System.Drawing.Point(605, 190);
+            this.mskTelefone.Mask = "(00)00000-0000";
+            this.mskTelefone.Name = "mskTelefone";
+            this.mskTelefone.Size = new System.Drawing.Size(92, 20);
+            this.mskTelefone.TabIndex = 7;
+            this.mskTelefone.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mskTelefone_MaskInputRejected);
+            this.mskTelefone.TextChanged += new System.EventHandler(this.mskTelefone_TextChanged);
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigo.ForeColor = System.Drawing.Color.Black;
+            this.lblCodigo.Location = new System.Drawing.Point(22, 41);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(50, 13);
+            this.lblCodigo.TabIndex = 8;
+            this.lblCodigo.Text = "Código:";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(22, 88);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(43, 13);
+            this.lblNome.TabIndex = 9;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblCpf
+            // 
+            this.lblCpf.AutoSize = true;
+            this.lblCpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCpf.Location = new System.Drawing.Point(538, 88);
+            this.lblCpf.Name = "lblCpf";
+            this.lblCpf.Size = new System.Drawing.Size(34, 13);
+            this.lblCpf.TabIndex = 10;
+            this.lblCpf.Text = "CPF:";
+            // 
+            // lblCep
+            // 
+            this.lblCep.AutoSize = true;
+            this.lblCep.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCep.Location = new System.Drawing.Point(22, 137);
+            this.lblCep.Name = "lblCep";
+            this.lblCep.Size = new System.Drawing.Size(35, 13);
+            this.lblCep.TabIndex = 11;
+            this.lblCep.Text = "CEP:";
+            // 
+            // lblEndereco
+            // 
+            this.lblEndereco.AutoSize = true;
+            this.lblEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEndereco.Location = new System.Drawing.Point(197, 141);
+            this.lblEndereco.Name = "lblEndereco";
+            this.lblEndereco.Size = new System.Drawing.Size(65, 13);
+            this.lblEndereco.TabIndex = 12;
+            this.lblEndereco.Text = "Endereço:";
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero.Location = new System.Drawing.Point(22, 186);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(54, 13);
+            this.lblNumero.TabIndex = 13;
+            this.lblNumero.Text = "Número:";
+            // 
+            // lblComplemento
+            // 
+            this.lblComplemento.AutoSize = true;
+            this.lblComplemento.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComplemento.Location = new System.Drawing.Point(176, 190);
+            this.lblComplemento.Name = "lblComplemento";
+            this.lblComplemento.Size = new System.Drawing.Size(86, 13);
+            this.lblComplemento.TabIndex = 14;
+            this.lblComplemento.Text = "Complemento:";
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefone.Location = new System.Drawing.Point(538, 193);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(61, 13);
+            this.lblTelefone.TabIndex = 15;
+            this.lblTelefone.Text = "Telefone:";
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDados.Location = new System.Drawing.Point(12, 288);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.Size = new System.Drawing.Size(776, 150);
+            this.dgvDados.TabIndex = 16;
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGravar.Location = new System.Drawing.Point(98, 246);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(75, 23);
+            this.btnGravar.TabIndex = 17;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(217, 246);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 18;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(341, 246);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 19;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(469, 246);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 20;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // btnExibir
+            // 
+            this.btnExibir.Location = new System.Drawing.Point(587, 246);
+            this.btnExibir.Name = "btnExibir";
+            this.btnExibir.Size = new System.Drawing.Size(75, 23);
+            this.btnExibir.TabIndex = 21;
+            this.btnExibir.Text = "Exibir";
+            this.btnExibir.UseVisualStyleBackColor = true;
+            this.btnExibir.Click += new System.EventHandler(this.btnExibir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExibir);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.dgvDados);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.lblComplemento);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.lblEndereco);
+            this.Controls.Add(this.lblCep);
+            this.Controls.Add(this.lblCpf);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblCodigo);
+            this.Controls.Add(this.mskTelefone);
+            this.Controls.Add(this.txtComplemento);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.mskCep);
+            this.Controls.Add(this.mskCpf);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtCodigo);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Clientes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.MaskedTextBox mskCpf;
+        private System.Windows.Forms.MaskedTextBox mskCep;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.TextBox txtComplemento;
+        private System.Windows.Forms.MaskedTextBox mskTelefone;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblCpf;
+        private System.Windows.Forms.Label lblCep;
+        private System.Windows.Forms.Label lblEndereco;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Label lblComplemento;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.DataGridView dgvDados;
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnExibir;
+    }
+}
+
---
+++ CadCliente/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace CadCliente
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conexao;
+        MySqlCommand comando;
+        MySqlDataAdapter da;
+        MySqlDataReader dr;
+        string strSQL;
+        string mensagem = "";
+        int confirmaCpf;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            //Mensagem de validação do CPF 
+            string valor = mskCpf.Text;
+            if (CadCliente.ValidaCpf.IsCpf(valor))
+            {
+                mensagem = "O número é um CPF Válido !";
+                confirmaCpf = 1;
+            }
+            else
+            {
+                mensagem = "O número é um CPF Inválido !";
+                confirmaCpf = 0;
+            }
+            MessageBox.Show(mensagem, "Validação");
+
+            try
+            {
+                if (confirmaCpf == 1)
+                {
+
+                    conexao = new MySqlConnection("Server=localhost;Database=cadClientebd;Uid=root;Pwd=KatiaJT38;");
+
+                    //Inserção dos dados
+                    strSQL = "INSERT INTO CLIENTE (NOME, CPF, CEP, ENDERECO, NUMERO, COMPLEMENTO, TELEFONE) " +
+                        "VALUES (@NOME, @CPF, @CEP, @ENDERECO, @NUMERO, @COMPLEMENTO, @TELEFONE)";
+
+                    comando = new MySqlCommand(strSQL, conexao);
+                    comando.Parameters.AddWithValue("@NOME", txtNome.Text);
+                    comando.Parameters.AddWithValue("@CPF", mskCpf.Text);
+                    comando.Parameters.AddWithValue("@CEP", mskCep.Text);
+                    comando.Parameters.AddWithValue("@ENDERECO", txtEndereco.Text);
+                    comando.Parameters.AddWithValue("@NUMERO", txtNumero.Text);
+                    comando.Parameters.AddWithValue("@COMPLEMENTO", txtComplemento.Text);
+                    comando.Parameters.AddWithValue("@TELEFONE", mskTelefone.Text);
+
+                    //Desabilita os campos do formulário após inserir/gravar os dados
+                    txtCodigo.Text = "";
+                    txtNome.Text = "";
+                    mskCpf.Text = "";
+                    mskCep.Text = "";
+                    txtEndereco.Text = "";
+                    txtNumero.Text = "";
+                    txtComplemento.Text = "";
+                    mskTelefone.Text = "";
+
+                    conexao.Open();
+
+                    comando.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+                comando = null;
+                MessageBox.Show("Cliente inserido com sucesso!");
+            }
+        }
+
+
+        //Mascara para seleção de número de telefone celular ou fixo
+        private void mskTelefone_TextChanged(object sender, EventArgs e)
+        {
+            if ("9".Equals(mskTelefone.Text.Substring(4, 1)))
+            {
+                mskTelefone.Mask = "(00)00000-0000";
+            }
+            else
+            {
+                mskTelefone.Mask = "(00)0000-0000";
+            }
+        }
+
+        private void mskTelefone_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            if (mskTelefone.Text.Length != mskTelefone.Mask.Length)
+                mskTelefone.ResetText();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new MySqlConnection("Server=localhost;Database=cadClientebd;Uid=root;Pwd=KatiaJT38;");
+
+                strSQL = "UPDATE CLIENTE SET NOME = @NOME, CPF = @CPF, CEP = @CEP, ENDERECO = @ENDERECO, " +
+                    "NUMERO = @NUMERO, COMPLEMENTO = @COMPLEMENTO, TELEFONE = @TELEFONE WHERE CODIGO = @CODIGO";
+                  
+                comando = new MySqlCommand(strSQL, conexao);
+                comando.Parameters.AddWithValue("@CODIGO", txtCodigo.Text);
+                comando.Parameters.AddWithValue("@NOME", txtNome.Text);
+                comando.Parameters.AddWithValue("@CPF", mskCpf.Text);
+                comando.Parameters.AddWithValue("@CEP", mskCep.Text);
+                comando.Parameters.AddWithValue("@ENDERECO", txtEndereco.Text);
+                comando.Parameters.AddWithValue("@NUMERO", txtNumero.Text);
+                comando.Parameters.AddWithValue("@COMPLEMENTO", txtComplemento.Text);
+                comando.Parameters.AddWithValue("@TELEFONE", mskTelefone.Text);
+
+                conexao.Open();
+
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+                comando = null;
+                MessageBox.Show("Cliente editado com sucesso!");
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new MySqlConnection("Server=localhost;Database=cadClientebd;Uid=root;Pwd=KatiaJT38;");
+
+                strSQL = "DELETE FROM CLIENTE WHERE CODIGO = @CODIGO";
+
+                comando = new MySqlCommand(strSQL, conexao);
+                comando.Parameters.AddWithValue("@CODIGO", txtCodigo.Text);
+                
+                conexao.Open();
+
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+                comando = null;
+                MessageBox.Show("Cliente excluído com sucesso!");
+            }
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new MySqlConnection("Server=localhost;Database=cadClientebd;Uid=root;Pwd=KatiaJT38;");
+
+                strSQL = "SELECT * FROM CLIENTE WHERE CODIGO = @CODIGO";
+
+                comando = new MySqlCommand(strSQL, conexao);
+                comando.Parameters.AddWithValue("@CODIGO", txtCodigo.Text);
+
+                conexao.Open();
+
+                dr = comando.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    txtNome.Text = Convert.ToString(dr["nome"]);
+                    mskCpf.Text = Convert.ToString(dr["cpf"]);
+                    mskCep.Text = Convert.ToString(dr["cep"]);
+                    txtEndereco.Text = Convert.ToString(dr["endereco"]);
+                    txtNumero.Text = Convert.ToString(dr["numero"]);
+                    txtComplemento.Text = Convert.ToString(dr["complemento"]);
+                    mskTelefone.Text = Convert.ToString(dr["telefone"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+                comando = null;
+                MessageBox.Show("Os dados do cliente " + txtCodigo.Text + " apresentado com sucesso!");
+            }
+        }
+
+        private void btnExibir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new MySqlConnection("Server=localhost;Database=cadClientebd;Uid=root;Pwd=KatiaJT38;");
+
+                strSQL = "SELECT * FROM CLIENTE";
+
+                da = new MySqlDataAdapter(strSQL, conexao);
+
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+
+                dgvDados.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+                comando = null;
+                MessageBox.Show("Os dados de todos os clientes apresentados com sucesso!");
+            }
+        }
+                
+    }
+}
